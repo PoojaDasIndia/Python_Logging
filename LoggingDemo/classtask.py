df3325531f14f3082d331e5681b79b2532c16dcd
Pooja <Poojadas93@outlook.com>
2022-07-18 18:01:52 +0530

    changing loggin function in class

--- LoggingDemo/classtask.py
+++ LoggingDemo/classtask.py
@@ -1,6 +1,13 @@
 import os
 import logging
 from datetime import date
+# logger=logging.getLogger()
+# logger.setLevel(logging.DEBUG)
+# ch=logging.StreamHandler()
+# formatter=logging.Formatter('%(asctime)s-%(levelname)s-%(message)s', datefmt='%d-%B-%y|%A| %H:%M:%S %p')
+# ch.setFormatter(formatter)
+# logger.addHandler(ch)
+
 class data:
 
     def __init__(self, file_name, file_type, dt, size):
@@ -8,16 +15,17 @@ class data:
         self.file_type=file_type
         self.date=dt
         self.size=size
-        
+
     @staticmethod
     def log(msg):
         os.system('cls')
-        logging.getLogger().setLevel(logging.DEBUG)
+        logger = logging.getLogger()
+        logger.setLevel(logging.DEBUG)
         ch=logging.StreamHandler()
-        formatter=logging.Formatter('%(asctime)s-%(levelname)s-%(message)s', datefmt='%d-%B-%y|%A|')
+        formatter=logging.Formatter('%(asctime)s-%(levelname)s-%(message)s', datefmt='%d-%B-%y|%A| %H:%M:%S %p')
         ch.setFormatter(formatter)
-        logging.getLogger().addHandler(ch)
-        logging.info(msg)
+        logger.addHandler(ch)
+        logger.info(msg)
 
     def File_open(self):
         
@@ -26,29 +34,39 @@ class data:
             file.write("Hello Python pooja das")
             
         except Exception as e:
-            self.log(e)
+            return self.log(e)
+            # logger.error(e)
+            # print(e)
+
             
     def File_read(self):
         try:
             file = open(self.file_name+"."+self.file_type, "r")
             file.seek(0)
-            self.log(file.read())
+            return self.log(file.read())
+            # logger.info(file.read())
+            # print(file.read())
         except Exception as e:
-            self.log(e)
+            return self.log(e)
+            # logger.error(e)
+            # print(e)
     
     def File_append(self):
         try:
             file= open(self.file_name+"."+self.file_type, "a")
-            file.write("hii. test Hello Python")
-            
+            file.write("hello 123 test Hello Python")
+
+
         except Exception as e:
-            self.log(e)
+            return self.log(e)
+            # logger.error(e)
+            # print(e)
 
 
-d=data("logs\\python", "txt", date.today(), 52)
+d=data(r"C:\Users\HP\Desktop\Python\Python_Logging\logs\python", "txt", date.today(), 52)
 
 d.File_open()
 d.File_read()
 d.File_append()
-
+d.File_read()
 
